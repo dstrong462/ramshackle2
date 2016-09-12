76cb643b734d7b415abd3fa30ac009011744ba45
Daniel Strong <danielpstrong@gmail.com>
2016-09-11 21:27:32 -0400

    added modal popup class with javascript

--- js/app.js
+++ js/app.js
@@ -1,13 +1,31 @@
 // Fade out and then remove notifications on click
 
-var closeButton = document.querySelectorAll('.close');
+var closeButtons = document.querySelectorAll('.close');
 
-for (var i = 0; i < closeButton.length; i++) {
-    closeButton[i].addEventListener('click', function() {
+for (var i = 0; i < closeButtons.length; i++) {
+    closeButtons[i].addEventListener('click', function() {
+        overlay.style.display = 'none';
         this.parentNode.style.opacity = '0';
         var temp = this;
         setTimeout(function() {
             temp.parentNode.style.display = 'none';
         }, 500);
     });
+}
+
+
+// Open modal window and overlay on click
+
+var overlay = document.getElementById('overlay');
+var modals = document.querySelectorAll('.modal');
+var modalButtons = document.querySelectorAll('.modal + button');
+
+for (var i = 0; i < modalButtons.length; i++) {
+    (function(i) {
+        modalButtons[i].addEventListener('click', function() {
+        overlay.style.display = 'inline';
+        modals[i].style.display = 'inline';
+        modals[i].style.opacity = '1';
+        });
+    })(i);
 }
